8704f75148bcf9c97da4937bbaffcff684865673
Anna Moraco <annamoraco@gmail.com>
2021-08-23 20:02:39 -0300

    Implementando com while

--- advinhacao.py
+++ advinhacao.py
@@ -3,18 +3,25 @@ print("Bem vindo ao jogo da advinhação!")
 print("********************************")
 
 numero_secreto = 45
+total_tentativas = 5
+rodada = 1
 
-chute = int(input("Digite um número: "))
+while rodada <= total_tentativas :
 
-print("Você digitou:",chute)
+    print(f"Tentativa {rodada} de {total_tentativas}")
+    chute = int(input("Digite um número: "))
+    print("Você digitou:",chute)
 
-acertou = chute == numero_secreto
+    acertou = chute == numero_secreto
 
-if (acertou):
-    print("Você acertou!")
-elif (chute > numero_secreto):
-    print("Você errou. O número secreto é menor.")
-else:
-    print("Você errou. O número secreto é maior.")
+    if (acertou):
+        print("Parabéns, você ganhou!")
+        break
+    elif (chute > numero_secreto):
+        print("Você errou. O número secreto é menor.")
+    else:
+        print("Você errou. O número secreto é maior.")
+
+    rodada = rodada + 1
 
 print("Fim de jogo")
