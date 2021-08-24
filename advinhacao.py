aa94b163013175edf8d2dcb018cfa94b64e67367
Anna Moraco <annamoraco@gmail.com>
2021-08-23 21:09:30 -0300

    Adicionando dificuldade e pontuação

--- advinhacao.py
+++ advinhacao.py
@@ -4,8 +4,22 @@ print("********************************")
 print("Bem vindo ao jogo da advinhação!")
 print("********************************")
 
+nivel = 0
+
+while (nivel < 1 or nivel > 3):
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil  (2) Médio  (3) Difícil")
+    nivel = int(input())
+
+    if nivel == 1:
+        total_tentativas = 50
+    elif nivel == 2:
+        total_tentativas = 25
+    elif nivel == 3:
+        total_tentativas = 10
+
 numero_secreto = random.randrange(0, 101)
-total_tentativas = 15
+pontos = 1000
 
 for rodada in range(1,total_tentativas + 1) :
 
@@ -19,10 +33,14 @@ for rodada in range(1,total_tentativas + 1) :
 
     if (acertou):
         print("Parabéns, você ganhou!")
+        print(f"Você fez {pontos} pontos")
         break
-    elif (chute > numero_secreto):
-        print("Você errou. O número secreto é menor.")
     else:
-        print("Você errou. O número secreto é maior.")
+        if (chute > numero_secreto):
+            print("Você errou. O número secreto é menor.")
+        else:
+            print("Você errou. O número secreto é maior.")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim de jogo")
